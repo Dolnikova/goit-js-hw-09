bca5840ab84fc1430d8ffed4182cfef62c08a1d0
Dolnikova <98531699+Dolnikova@users.noreply.github.com>
2022-08-14 21:50:51 +0000

    Deploying to gh-pages from @ Dolnikova/goit-js-hw-09@481c4eca88da97c66d56d423ae653af8472cd976 🚀

---
+++ 01-color-switcher.c9c5805b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.body;t.addEventListener("click",(()=>{timerId=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(timerId),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.c9c5805b.js.map
