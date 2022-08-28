3ab84af262b16b442fa89aaedad054e553820ca7
Iuliia Dolnikova <dolnikova80@gmail.com>
2022-08-28 12:39:06 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,21 +1,22 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 const body = document.body;
-
+stopBtn.disabled = true;
+let timerId = null;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-btnStop.disabled = true;
-let timerId = null;
+
 startBtn.addEventListener('click', () => {
   timerId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
-  btnStop.disabled = false;
+  stopBtn.disabled = false;
   startBtn.disabled = true;
 });
 
 stopBtn.addEventListener('click', () => {
   clearInterval(timerId);
   startBtn.disabled = false;
+  stopBtn.disabled = true;
 });
