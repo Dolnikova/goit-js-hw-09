009b4336170d952c3a918c21c316ffd354678b7e
Dolnikova <98531699+Dolnikova@users.noreply.github.com>
2022-08-28 10:37:58 +0000

    Deploying to gh-pages from @ Dolnikova/goit-js-hw-09@0b1b0c728a8f5302edb8e4ac7aa7d9fdb84d4e99 🚀

---
+++ 01-color-switcher.21d79b06.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=document.body;btnStop.disabled=!0;var o=null;t.addEventListener("click",(function(){o=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),btnStop.disabled=!1,t.disabled=!0})),n.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.21d79b06.js.map
